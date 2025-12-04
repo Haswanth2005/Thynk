5fba2e84f3653c6e0de45e9214a246fa0b7708ea
Haswanth2005 <makthalahaswanth@gmail.com>
2025-12-04 13:17:13 +0530

    project setup

---
+++ frontend/src/components/navbar.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+
+      <Link>
+        <img src="" alt="" />
+      </Link>
+
+    </div>
+  )
+}
+
+export default Navbar
